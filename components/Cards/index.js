16d01852a3909dc39101bb8693d080a9b8653fca
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-21 21:56:21 -0500

    partial code for article cards. not completed.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+// https://lambda-times-backend.herokuapp.com/articles
+
+// data.articles.type
+function articleMaker(obj){
+    let articleMainContainer = document.querySelector('.cards-container');
+
+    let articleSmallContainer = document.createElement('div');
+    articleSmallContainer.classList.add('card');
+    articleMainContainer.appendChild(articleSmallContainer);
+
+    let articleHeadline = document.createElement('div');
+    articleHeadline.classList.add('headline');
+    articleHeadline.textContent = obj.headline;
+    articleMainContainer.appendChild(articleHeadline);
+
+    let articleAuthorContainer = document.createElement('div');
+    articleAuthorContainer.classList.add('author');
+    articleMainContainer.appendChild(articleAuthorContainer);
+
+    let authorImg = document.appendChild('img');
+    authorImg.setAttribute('src', `${obj.authorPhoto}`);
+    articleMainContainer.appendChild(authorImg);
+
+    let authorName = document.createElement('span');
+    authorName.textContent = `By: ${obj.authorName}`
+    articleMainContainer.appendChild(authorName);
+
+    return articleMainContainer;
+
+}
+// console.log(articleMaker());
+
+
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then((response) =>  {
+        response.data.articles.javascript.forEach((item => {
+            articleMaker(item);
+        }))
+        // console.log(response.data.articles.javascript);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
